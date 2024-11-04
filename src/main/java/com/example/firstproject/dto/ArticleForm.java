2aae136cd2ce78fbd7f6f277659cb7567e6af1d2
MoolJomdao <sasori1602@naver.com>
2024-11-04 21:34:25 +0900

    세번째 랜덤으로 명언 출력, 게시판 시작[글 생성 폼]

---
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -0,0 +1,41 @@
+package com.example.firstproject.dto;
+
+import java.beans.ConstructorProperties;
+
+public class ArticleForm {
+    private String title;
+    private String content;
+
+    public ArticleForm(){}
+
+    public ArticleForm(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "ArticleForm{" +
+                "title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+
+
+}
