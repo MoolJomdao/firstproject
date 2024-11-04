66c3fd57b1b5dfc8853af590150830513e6fabdc
MoolJomdao <sasori1602@naver.com>
2024-11-04 21:40:09 +0900

    세번째 랜덤으로 명언 출력, 게시판 시작[글 생성 폼]

--- src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -2,6 +2,8 @@ package com.example.firstproject.dto;
 
 import java.beans.ConstructorProperties;
 
+//기본 생성자, Getter, Setter 무조건 추가 해줘야 작동함 시발꺼 책에서 이걸 안가르쳐주네 (2024/11/04 Hwang.Angry
+
 public class ArticleForm {
     private String title;
     private String content;
